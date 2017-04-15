ea22de82792d69923f3e0f9f8d5505ed2effc8c5
Sam Trenholme <strenholme.usenet@gmail.com>
2017-04-15 02:08:12 -0700

    Fix SQA test compare (-ne is numbers != is strings in SH)

--- sqa/do.test.sh
+++ sqa/do.test.sh
@@ -4,7 +4,7 @@ cp ../map_gen.ini .
 ../map_gen -1
 MD5=$( md5sum "nav_req.cevo map" | awk '{print $1}' )
 
-if [ "$MD5" -ne "7753c0263aabd4d304de6eb98d1e4cfa" ] ; then
+if [ "$MD5" != "7753c0263aabd4d304de6eb98d1e4cfa" ] ; then
 	echo Map gen test FAIL
 	exit 1
 fi
